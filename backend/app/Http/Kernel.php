b75364bfb5d49284418b3c528603b58545a3324d
Tenny N <tenny@mediawave.co.id>
2026-03-11 15:31:25 +0700

    multi company 13

--- backend/app/Http/Kernel.php
+++ backend/app/Http/Kernel.php
@@ -41,6 +41,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
+            \App\Http\Middleware\SetTenantFromSession::class,
             // \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
@@ -62,5 +63,6 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'tenant' => \App\Http\Middleware\SetTenantFromSession::class,
     ];
 }
